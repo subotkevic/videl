ce9fe15e2c69c5c2600f0dab4d29fa7b1d79dca2
Artur Subotkevič <artur@subotkevic.com>
2017-03-24 06:09:28 +0200

    Properties for Columns component

--- src/components/Columns/Columns.vue
+++ src/components/Columns/Columns.vue
@@ -1,11 +1,27 @@
 <template>
-  <div class="columns">
+  <div class="columns" :class="classes">
     <slot></slot>
   </div>
 </template>
 
 <script>
   export default {
+    props: {
+      'is-mobile': Boolean,
+      'is-desktop': Boolean,
+      'is-multiline': Boolean,
+      'is-gapless': Boolean,
+    },
 
+    computed: {
+      classes() {
+        return {
+          'is-mobile': this.isMobile,
+          'is-desktop': this.isDesktop,
+          'is-multiline': this.isMultiline,
+          'is-gapless': this.isGapless,
+        }
+      }
+    }
   }
 </script>
